f3058ff5eabbe2e4fc597e7d9ab4078876859733
LMaurer <121942116+Lauriane-Maurer@users.noreply.github.com>
2023-05-23 12:23:36 +0200

    Modification Mdp

--- src/main/java/fr/simplon/titrev3/ControllerClient/UserController.java
+++ src/main/java/fr/simplon/titrev3/ControllerClient/UserController.java
@@ -1,12 +1,15 @@
 package fr.simplon.titrev3.ControllerClient;
 
 
+import fr.simplon.titrev3.dto.ChangePasswordForm;
 import fr.simplon.titrev3.dto.CreateUserForm;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,11 +22,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.sql.DataSource;
+import java.security.Principal;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -96,5 +98,57 @@ public class UserController {
     public String afficherPageGestionnaireAdmin(Model model)
     {
         return "pageAdmin";
+    }
+
+
+
+
+    @GetMapping("/change-password")
+    public String changePassword(Model model)
+    {
+        ChangePasswordForm userForm = new ChangePasswordForm();
+        model.addAttribute("user", userForm);
+        return "modificationMdp";
+    }
+
+    @PostMapping("/change-password")
+    public String changePassword(
+            Principal principal,
+            @ModelAttribute("user") @Valid ChangePasswordForm user,
+            BindingResult validation,
+            Model model) {
+        if (principal == null) {
+            return "redirect:/confirmationChangementMdp";
+        }
+        UserDetails userDetails = userDetailsManager.loadUserByUsername(principal.getName());
+        changePassword(userDetails, user, validation);
+        if (validation.hasErrors()) {
+            return "modificationMdp";
+        }
+        return "redirect:/confirmationChangementMdp";
+    }
+
+    private void changePassword(UserDetails userDetails, ChangePasswordForm form, BindingResult validation) {
+        if (userDetails == null) {
+            validation.addError(new FieldError("user", "username", "Utilisateur inconnu"));
+        } else {
+            String currentPassword = userDetails.getPassword();
+
+            if (!passwordEncoder.matches(form.getOldPassword(), currentPassword)) {
+                validation.addError(new FieldError("user", "oldPassword", "Mot de passe invalide"));
+            }
+            if (!form.getNewPassword().equals(form.getConfirmPassword())) {
+                validation.addError(new FieldError("user", "confirmPassword", "Les mots de passe ne correspondent pas"));
+            }
+            if (!validation.hasErrors()) {
+                userDetailsManager.changePassword(form.getOldPassword(), passwordEncoder.encode(form.getNewPassword()));
+            }
+        }
+    }
+
+    @GetMapping("/confirmationChangementMdp")
+    public String confirmationChangePassword()
+    {
+        return "confirmationChangementMdp";
     }
 }
---
+++ src/main/java/fr/simplon/titrev3/dto/ChangePasswordForm.java
@@ -0,0 +1,45 @@
+package fr.simplon.titrev3.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public class ChangePasswordForm {
+
+    private String username;
+    private String oldPassword;
+    @NotNull
+    private String newPassword;
+    @NotNull
+    private String confirmPassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
